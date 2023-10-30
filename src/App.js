2debcad33773becb0724868b48e2800775533104
Ahmad <heyahmad611@gmail.com>
2023-10-30 23:04:41 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Departments from './components/Departments';
-import What from './components/What';
+import "./App.css";
+import Departments from "./components/Departments";
+import What from "./components/What";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <h1>E-CELL</h1>
       {/* <What /> */}
-      <Departments/>
+      <div className="content-container">
+        {/* <Departments /> */}
+      </div>
+      <hr/>
+      <Footer />
+      
     </>
   );
 }
