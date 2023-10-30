abdf89835603f1ce7583c1be91543fe3973ce20a
Gourab Bistu <gourabbistu2002@gmail.com>
2023-10-30 13:39:54 +0530

    Added Department Section and what to do part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Departments from './components/Departments';
+import What from './components/What';
 
 function App() {
   return (
     <>
       <h1>E-CELL</h1>
+      {/* <What /> */}
+      <Departments/>
     </>
   );
 }
---
+++ src/components/What.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+export default function Navbar(props) {
+  return (
+    <section id="whatwedo">
+      {/* what to do part */}
+    <div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5  whatwedo" >WHAT WE DO ?</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4 work">We help students in developing their entrepreneurship skills & create enthusiasm towards the start-up ecosystem by means of engaging activities like Guest Lectures, Panel Discussions, Business Plan/Model and Idea Pitching sessions. E-Cell NIT Jamshedpur is a team of hardworking and goal oriented individuals, devoted to empowering students in field of entrepreneurship.</p>
+    </div>
+  </div>
+  
+    </section>
+  
+  )};
